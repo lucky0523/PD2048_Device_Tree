078072f1ae9e9ad0f26a9ad6963ae3021b9e2fa8
lucky0523 <lucky0523@gmail.com>
2023-12-10 03:08:00 +0800

    Update twrp_PD2048.mk

--- twrp_PD2048.mk
+++ twrp_PD2048.mk
@@ -10,13 +10,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from PD2048 device
 $(call inherit-product, device/vivo/PD2048/device.mk)
 
 PRODUCT_DEVICE := PD2048
-PRODUCT_NAME := omni_PD2048
+PRODUCT_NAME := twrp_PD2048
 PRODUCT_BRAND := vivo
 PRODUCT_MODEL := V2048A
 PRODUCT_MANUFACTURER := vivo
